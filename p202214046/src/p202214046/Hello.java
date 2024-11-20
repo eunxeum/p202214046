6b10edc4a6deae4ad745561fe8af5f68d220286f
Lee, yeeun <ye7426@naver.com>
2024-11-20 10:02:09 +0900

    two

---
+++ p202214046/src/p202214046/Hello.java
@@ -0,0 +1,12 @@
+package p202214046;
+
+public class Hello {
+
+    public static int sum(int n) {
+      int result = 0;
+      for (int i = 1; i <= n; ++i)
+        result += i;
+      return result;
+    }
+  }
+  
--- p202214046/src/p202214046/Main.java
+++ p202214046/src/p202214046/Main.java
@@ -1,8 +1,11 @@
 package p202214046;
 
 public class Main {
-	  public static void main(String[] args) {
-	    //  자신의 학번과 이름을 출력하라
-	    System.out.println("201514099 홍길동");
-	  }
+	public static void main(String[] args) {
+	  // 자신의 학번과 이름을 입력하라
+	  System.out.println("201514099 홍길동");
+	  System.out.println(Hello.sum(10));
+	  System.out.println(Hello.sum(100));
 	}
+  }
+  
